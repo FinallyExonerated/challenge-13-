3ec7b8e7fee8ad3a4665c50451aeb23658fd51bc
exonerated <jordansmithofficially@gmail.com>
2022-11-11 18:22:27 -0500

    fixes

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      Decimal:
+      allowNull: false,
+      valueDecimal:
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      valueNumeric:
+    },
+    category_id: {
+      type: DataTypes.INTEGER
+
+    }
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,19 +3,19 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+// router.get('/', (req, res) => {
+//   // find all categories
+//   // be sure to include its associated Products
+// });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+// router.get('/:id', (req, res) => {
+//   // find one category by its `id` value
+//   // be sure to include its associated Products
+// });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+// router.post('/', (req, res) => {
+//   // create a new category
+// });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
@@ -23,6 +23,64 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single location
+router.get('/:id', async (req, res) => {
+  try {
+    const locationData = await Location.findByPk(req.params.id, {
+      // JOIN with travellers, using the Trip through table
+      include: [{ model: Traveller, through: Trip, as: 'location_travellers' }]
+    });
+
+    if (!locationData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// CREATE a location
+router.post('/', async (req, res) => {
+  try {
+    const locationData = await Location.create(req.body);
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// DELETE a location
+router.delete('/:id', async (req, res) => {
+  try {
+    const locationData = await Location.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!locationData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
